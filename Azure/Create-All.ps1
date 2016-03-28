2c4cece2da7d785b9b0d7fbdea1f818be87ae3c8
Rob Garrett <rob@robgarrett.com>
2016-03-28 17:49:33 -0400

    Web App Create and Teardown

---
+++ Azure/Create-All.ps1
@@ -0,0 +1,35 @@
+﻿[CmdletBinding()]Param();
+
+$global:rgName = "Study";
+$global:location = "East US";
+
+############################### Resource Group First ###############################
+##
+if ((Get-AzureRmResourceGroup -Name $global:rgName -ErrorAction SilentlyContinue) -eq $null) {
+    Write-Verbose "Creating Resource Group";
+    New-AzureRmResourceGroup -Name $global:rgName -Location $global:location;
+}
+
+############################### Web Apps ###############################
+##
+$global:webAppName = "RobDev-TestWeb";
+if ((Get-AzureRmAppServicePlan -ResourceGroupName $global:rgName -Name "$($global:webAppName)-ASP" -ErrorAction SilentlyContinue) -eq $null) {
+    Write-Verbose "Creating App Service Plan";
+    New-AzureRMAppServicePlan -Name "$($global:webAppName)-ASP" -Location $global:location -ResourceGroup $global:rgName -Tier Standard;
+}
+
+$webApp = Get-AzureRmWebApp -ResourceGroupName $global:rgName -Name $global:webAppName -ErrorAction SilentlyContinue;
+if ($webApp -eq $null) {
+    Write-Verbose "Creating Web App";
+    New-AzureRMWebApp -Name $global:webAppName -Location $global:location -ResourceGroup $global:rgName -AppServicePlan "$($global:webAppName)-ASP";    
+}
+
+Write-Host -ForegroundColor Yellow "WebSite Address is: http://$($webApp.EnabledHostNames[0])";
+if ((Get-AzureRmWebAppSlot -ResourceGroupName $global:rgName -Name $global:webAppName -Slot "staging" -ErrorAction SilentlyContinue) -eq $null) {
+    Write-Verbose "Creating Staging Slot";
+    New-AzureRmWebAppSlot -ResourceGroupName $global:rgName -Name $global:webAppName -Slot "staging" -AppServicePlan "$($global:webAppName)-ASP";
+}
+
+Write-Verbose "Running a test swap with staging";
+$ParametersObject = @{targetSlot  = "production"}
+Invoke-AzureRmResourceAction -ResourceGroupName $global:rgName -ResourceType Microsoft.Web/sites/slots -ResourceName "$($global:webAppName)/staging" -Action slotsswap -Parameters $ParametersObject;
---
+++ Azure/Delete-All.ps1
@@ -0,0 +1,31 @@
+﻿[CmdletBinding()]Param();
+
+$global:rgName = "Study";
+$global:location = "East US";
+
+############################### Web Apps ###############################
+##
+$global:webAppName = "RobDev-TestWeb";
+$webApp = Get-AzureRmWebApp -ResourceGroupName $global:rgName -Name $global:webAppName -ErrorAction SilentlyContinue;
+if ((Get-AzureRmWebAppSlot -ResourceGroupName $global:rgName -Name $global:webAppName -Slot "staging" -ErrorAction SilentlyContinue) -ne $null) {
+    Write-Verbose "Remove Staging Slot";
+    Remove-AzureRmWebAppSlot -ResourceGroupName $global:rgName -Name $global:webAppName -Slot "staging";
+}
+
+if ($webApp -ne $null) {
+    Write-Verbose "Removing Web App";
+    Remove-AzureRMWebApp -Name $global:webAppName -ResourceGroup $global:rgName -Force;    
+}
+
+if ((Get-AzureRmAppServicePlan -ResourceGroupName $global:rgName -Name "$($global:webAppName)-ASP" -ErrorAction SilentlyContinue) -ne $null) {
+    Write-Verbose "Removing App Service Plan";
+    Remove-AzureRMAppServicePlan -Name "$($global:webAppName)-ASP" -ResourceGroup $global:rgName -Force;
+}
+
+
+############################### Resource Group Last ###############################
+##
+if ((Get-AzureRmResourceGroup -Name $global:rgName -ErrorAction SilentlyContinue) -ne $null) {
+    Write-Verbose "Removing Resource Group";
+    Remove-AzureRmResourceGroup -Name $global:rgName -Force;
+}
