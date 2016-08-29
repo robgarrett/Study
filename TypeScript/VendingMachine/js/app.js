508ab98d235a4fb5b931ac03bf209780ae4b1696
Rob Garrett <rob@robdev.local>
2016-08-29 13:16:00 -0400

    Ignore generated JS files.

--- TypeScript/VendingMachine/js/app.js
+++
@@ -1,8 +0,0 @@
-var VendingMachine = (function () {
-    function VendingMachine() {
-    }
-    return VendingMachine;
-}());
-/// <reference path="vendingMachine.ts" />
-var machine = new VendingMachine();
-//# sourceMappingURL=app.js.map
