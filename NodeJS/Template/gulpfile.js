6766968d415c701086c7607f7bc5ca0f4d2240d1
Rob Garrett <rob@robgarrett.com>
2018-05-19 13:43:56 -0400

    Working on gulp update

--- NodeJS/Template/gulpfile.js
+++ NodeJS/Template/gulpfile.js
@@ -1,59 +1,60 @@
-var gulp   = require('gulp');
-var tsc    = require('gulp-tsc');
-var shell  = require('gulp-shell');
-var runseq = require('run-sequence');
+var gulp = require('gulp');
+var tsc = require('gulp-typescript');
+var gulpNSP = require('gulp-nsp');
+var shell = require('gulp-shell');
 var tslint = require('gulp-tslint');
+var flatten = require('gulp-flatten');
 var webpack = require('webpack');
 var webpackconfig = require('./webpack.config.dev.js');
 
 var paths = {
-  tscripts : { src : ['src/**/*.ts'], dest : 'lib' }
+  tscripts: {
+    src: [
+      'app/src/**/*.ts',
+      'app/build/**/*.ts'
+    ], dest: 'app/lib'
+  }
 };
 
-gulp.task('default', ['lint', 'buildrun']);
+//gulp.task('default', gulp.series('lint', 'buildrun'));
 
-// ** Running ** //
-gulp.task('run', shell.task([
-  'node lib/app.js'
-]));
-
-gulp.task('buildrun', function (cb) {
-  runseq('build', 'run', cb);
+// ** Linting ** //
+gulp.task('lint', function doWork() {
+  return gulp.src(paths.tscripts.src)
+    .pipe(tslint({ formatter: "verbose" }))
+    .pipe(tslint.report());
 });
 
+// ** Compilation ** //
+gulp.task('nsp:check', function doWork(done) {
+  gulpNSP({package: __dirname + '/package.json'}, done);
+});
+gulp.task('compile:typescript', function doWork() {
+  var project = tsc.createProject("tsconfig.json", { declaration: true });
+  var built = gulp.src(paths.tscripts.src).pipe(project());
+  return built.js
+    .pipe(flatten())
+    .pipe(gulp.dest(paths.tscripts.dest));
+});
+gulp.task('build', gulp.series('nsp:check', 'compile:typescript'));
+
+// ** Running ** //
+gulp.task('buildrun', function doWork() {
+  return gulp.series('build', 'run');
+});
+gulp.task('run', shell.task([
+  'node app/lib/serve.js'
+]));
+
+/*
 // ** Watching ** //
+gulp.task('watchrun', gulp.series('compile:typescript', 'run'));
 gulp.task('watch', function () {
   gulp.watch(paths.tscripts.src, ['compile:typescript']);
 });
 
-gulp.task('watchrun', function () {
-  gulp.watch(paths.tscripts.src, runseq('compile:typescript', 'run'));
-});
-
-// ** Compilation ** //
-gulp.task('build', ['nsp:check', 'compile:typescript']);
-gulp.task('nsp:check', function(){
-  'nsp check';
-});
-gulp.task('compile:typescript', function () {
-  return gulp.src(paths.tscripts.src)
-    .pipe(tsc({
-      lib: ["es6"],
-      emitError: false
-  }))
-  .pipe(gulp.dest(paths.tscripts.dest));
-});
-
-// ** Linting ** //
-gulp.task('lint', ['lint:default']);
-gulp.task('lint:default', function(){
-      return gulp.src(paths.tscripts.src)
-        .pipe(tslint({ formatter: "verbose"}))
-        .pipe(tslint.report());
-});
-
 // ** Packaging ** //
-gulp.task('package', ['lint', 'package:code']);
+gulp.task('package', gulp.series('lint', 'package:code'));
 gulp.task('package:code', ['build'], function (done) {
   webpack(webpackconfig, function (err, stats) {
     if (err) {
@@ -65,3 +66,4 @@ gulp.task('package:code', ['build'], function (done) {
     done();
   });
 });
+*/
