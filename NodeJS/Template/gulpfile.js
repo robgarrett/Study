a76818afc408ced72c6a4d5c631aa6072170eee4
Rob Garrett <rob@robgarrett.com>
2018-05-19 15:23:09 -0400

    Fixed gulp file

--- NodeJS/Template/gulpfile.js
+++ NodeJS/Template/gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var tsc = require('gulp-typescript');
 var gulpNSP = require('gulp-nsp');
-var shell = require('gulp-shell');
+var exec = require('gulp-exec');
 var tslint = require('gulp-tslint');
 var flatten = require('gulp-flatten');
+var clean = require('gulp-clean');
 var webpack = require('webpack');
 var webpackconfig = require('./webpack.config.dev.js');
 
@@ -11,12 +12,16 @@ var paths = {
   tscripts: {
     src: [
       'app/src/**/*.ts',
-      'app/build/**/*.ts'
+      'app/serve/**/*.ts'
     ], dest: 'app/lib'
   }
 };
 
-//gulp.task('default', gulp.series('lint', 'buildrun'));
+// ** Clean ** /
+gulp.task('clean', function doWork() {
+  return gulp.src(paths.tscripts.dest + '/*.js', {read:false})
+    .pipe(clean());
+});
 
 // ** Linting ** //
 gulp.task('lint', function doWork() {
@@ -36,34 +41,29 @@ gulp.task('compile:typescript', function doWork() {
     .pipe(flatten())
     .pipe(gulp.dest(paths.tscripts.dest));
 });
-gulp.task('build', gulp.series('nsp:check', 'compile:typescript'));
+gulp.task('build', gulp.series('clean', 'nsp:check', 'compile:typescript'));
 
 // ** Running ** //
-gulp.task('buildrun', function doWork() {
-  return gulp.series('build', 'run');
+gulp.task('run', function doWork() {
+  return gulp.src('./**/**').pipe(exec('node app/lib/serve.js'));
 });
-gulp.task('run', shell.task([
-  'node app/lib/serve.js'
-]));
+gulp.task('buildrun', gulp.series('build', 'run'));
 
-/*
 // ** Watching ** //
-gulp.task('watchrun', gulp.series('compile:typescript', 'run'));
-gulp.task('watch', function () {
-  gulp.watch(paths.tscripts.src, ['compile:typescript']);
+gulp.task('watch:code', function doWork() {
+  return watcher = gulp.watch(paths.tscripts.src, gulp.series('compile:typescript'));
 });
+gulp.task('watch', gulp.parallel('watch:code'));
 
 // ** Packaging ** //
-gulp.task('package', gulp.series('lint', 'package:code'));
-gulp.task('package:code', ['build'], function (done) {
+gulp.task('package:code', gulp.series('build', function (done) {
   webpack(webpackconfig, function (err, stats) {
-    if (err) {
-      console.error("package:code", err);
-    }
-    console.log(stats.toString({
-      colors: true
-    }));
+    if (err) { console.error("package:code", err); }
+    console.log(stats.toString({ colors: true }));
     done();
   });
-});
-*/
+}));
+gulp.task('package', gulp.series('lint', 'package:code'));
+
+// ** Default ** //
+gulp.task('default', gulp.series('lint', 'buildrun'));
