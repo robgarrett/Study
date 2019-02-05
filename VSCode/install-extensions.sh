6954d68693e042e586a084248f071795e211470c
Rob Garrett <rogarret@microsoft.com>
2019-02-05 17:46:28 -0500

    Added extensions script for VSCode

---
+++ VSCode/install-extensions.sh
@@ -0,0 +1,8 @@
+code --install-extension DotJoshJohnson.xml
+code --install-extension eg2.tslint
+code --install-extension ms-azuretools.vscode-azurefunctions
+code --install-extension ms-vscode.azure-account
+code --install-extension ms-vscode.azurecli
+code --install-extension ms-vscode.PowerShell
+code --install-extension msazurermtools.azurerm-vscode-tools
+code --install-extension PeterJausovec.vscode-docker
