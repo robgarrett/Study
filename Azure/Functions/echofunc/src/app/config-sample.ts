21a13c867ba3ffef2de3a0ff23a3347e0795a62c
Rob Garrett <rob@robgarrett.com>
2019-05-10 14:12:24 -0400

    Updated app to stock template

--- Azure/Functions/echofunc/src/app/config-sample.ts
+++
@@ -1,5 +0,0 @@
-export module OAuthConfig {
-  // Exports for OAUTH using client ID and secret.
-  export const clientId = "Add client id here";
-  export const clientSecret = "Add client secret here";
-}
