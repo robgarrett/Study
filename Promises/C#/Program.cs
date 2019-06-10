55fa4a82db1646ccc493a05a72db7bff4dab941a
Rob Garrett <rob@robgarrett.com>
2019-06-10 12:56:54 -0400

    Added C# example

---
+++ Promises/C#/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace C_
+{
+    class Program
+    {
+        static async Task RunAsync()
+        {
+            // Use an async method because main doesn't support it.
+            // Can await on any promise.
+            await Delay(2000).ContinueWith(task => {
+                Console.WriteLine("Hello World!");
+            });
+        }
+
+        static async Task Delay(int ms)
+        {
+            // Create a promise (TCS) and trigger promise after delay
+            // of ms milliseconds.
+            var tsc = new TaskCompletionSource<bool>();
+            var fireAndForgetTask = Task.Delay(ms).ContinueWith(task => tsc.SetResult(true));
+            // Execute the task async associated with the promise (TCS).
+            await tsc.Task;
+        }
+
+        static void Main(string[] args)
+        {
+            // Unlike TypeScript, we cannor make the main method
+            // async, which means we can't use "await" on the aync task.
+            // However, we can wait methods that return a Task object.
+            // Without the wait, execution would complete the finish step
+            // and fall out the bottom of the app before the async task completed.
+            Console.WriteLine("Started");
+            RunAsync().Wait();
+            Console.WriteLine("Finished");
+        }
+    }
+}
--- Promises/TypeScript/src/app/index.ts
+++ Promises/TypeScript/src/app/index.ts
@@ -1,15 +1,25 @@
 
 abstract class App {
 
-  public static main() {
-    App.delayAsync(2000).then(() => {
+  // TypeScript allows entrypoint methods as async.
+  // We wait on the RunAsync method so that the finish
+  // line runs last.
+  public static async main() {
+    console.log("Started");
+    await this.RunAsync();
+    console.log("Finished");
+  }
+
+  public static async RunAsync() {
+    // The await keyword ensures the promise
+    // completes before returning.
+    await App.delay(2000).then(() => {
       console.log("Hello World!");
     });
   }
 
   // Delay function to return a promise.
-  private static delayAsync = (ms: number) => new Promise(res => setTimeout(res, ms));
-
+  private static delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 }
 
 App.main();
