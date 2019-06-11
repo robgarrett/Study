20d1b989791a80893c7e53860f36061e4e941f0d
Rob Garrett <rob@robgarrett.com>
2019-06-11 07:31:50 -0400

    Figured out async/await (finally)

--- Promises/C#/Program.cs
+++ Promises/C#/Program.cs
@@ -6,34 +6,43 @@ namespace C_
 {
     class Program
     {
-        static async Task RunAsync()
+        static Task<bool> LegacyDelay(int ms)
         {
-            // Use an async method because main doesn't support it.
-            // Can await on any promise.
-            await DelayAsync(2000).ContinueWith(task => {
-                Console.WriteLine("Hello World!");
-            });
-        }
-
-        static Task DelayAsync(int ms)
-        {
-            // Create a promise (TCS) and trigger promise after delay
-            // of ms milliseconds.
+            // We use a TSC when wrapping legacy callback async methods
+            // that don't implement a task. For the sake of simplicity
+            // I'll use Task.Delay here. I could just as easy return 
+            // the result of Task.Delay, but want to demonstrate TSC.
             var tsc = new TaskCompletionSource<bool>();
-            var fireAndForgetTask = Task.Delay(ms).ContinueWith(task => tsc.SetResult(true));
-            // Execute the task async associated with the promise (TCS).
+            Task.Delay(ms).ContinueWith(Task => tsc.SetResult(true));
             return tsc.Task;
         }
 
-        static void Main(string[] args)
+        static async Task DelayAsync(int ms)
         {
-            // Unlike TypeScript, we cannot make the main method
-            // async, which means we can't use "await" on the aync task.
-            // However, we can wait methods that return a Task object.
-            // Without the wait, execution would complete the finish step
-            // and fall out the bottom of the app before the async task completed.
+            Console.WriteLine("Delay started");
+            // The await ensures that LegacyDelay completes before
+            // the rest of this method is executed. If not complete
+            // when called, execution is passed to the calling method
+            // and a continuation point is added for returning when
+            // LegacyDelay does complete.
+            await LegacyDelay(ms);
+            // Rest of code executed after LegacyDelay completes.
+            Console.WriteLine("Delay completed");
+        }
+
+        static async Task Main(string[] args)
+        {
+            // C# now supports async in Main.
             Console.WriteLine("Started");
-            RunAsync().Wait();
+            var delayTask = DelayAsync(2000).ContinueWith(task => {
+                Console.WriteLine("Hello World!");
+            });
+            // Control should pass back here while the delay is running.
+            Console.WriteLine("Doing other things...");
+            await delayTask;
+            // The above await ensures that the rest
+            // of this method doesn't run until the 
+            // DelayAsync method completes.
             Console.WriteLine("Finished");
         }
     }
--- Promises/TypeScript/src/app/index.ts
+++ Promises/TypeScript/src/app/index.ts
@@ -1,25 +1,33 @@
 
 abstract class App {
 
-  // TypeScript allows entrypoint methods as async.
-  // We wait on the RunAsync method so that the finish
-  // line runs last.
   public static async main() {
     console.log("Started");
-    await this.RunAsync();
+    const promise = this.DelayAsync(2000).then(() => {
+      console.log("Hello World!");
+    });
+    // Control should pass back here while the delay is running.
+    console.log("Doing other things...");
+    await promise;
+    // The above await ensures that the rest
+    // of this method doesn't run until the
+    // DelayAsync method completes.
     console.log("Finished");
   }
 
-  public static async RunAsync() {
-    // The await keyword ensures the promise
-    // completes before returning.
-    await App.delay(2000).then(() => {
-      console.log("Hello World!");
-    });
+  public static async DelayAsync(ms: number) {
+    console.log("Delay started");
+    // The await ensures that LegacyDelay completes before
+    // the rest of this method is executed. If not complete
+    // when called, execution is passed to the calling method
+    // and a continuation point is added for returning when
+    // LegacyDelay does complete.
+    await App.LegacyDelay(ms);
+    console.log("Delay completed");
   }
 
   // Delay function to return a promise.
-  private static delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+  private static LegacyDelay = (ms: number) => new Promise(res => setTimeout(res, ms));
 }
 
 App.main();
