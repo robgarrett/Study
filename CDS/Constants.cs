46093555c18e200f9513d74c13289c628d4d27e2
Rob Garrett <rob@robgarrett.com>
2020-07-20 18:57:03 -0400

    Attempting to connect to CDS

---
+++ CDS/Constants.cs
@@ -0,0 +1,8 @@
+namespace CDS
+{
+    public static class Constants
+    {
+        public const string ScopeUserRead = "User.Read";
+        public const string BearerAuthorizationScheme = "Bearer";
+    }
+}
