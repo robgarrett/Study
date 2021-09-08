a017b1d656d898f3d175f3d029d2cdcac6fa2030
Rob Garrett <rob@robgarrett.com>
2021-09-08 19:09:18 -0400

    Updating tasks

--- Codility/Task 01 - Binary Gap/Program.cs
+++ Codility/Task 01 - Binary Gap/Program.cs
@@ -1,23 +1,15 @@
 ﻿using System;
 
-namespace BinaryGap
+namespace Task01
 {
-    // you can also use other imports, for example:
-    // using System.Collections.Generic;
-
-    // you can write to stdout for debugging purposes, e.g.
-    // Console.WriteLine("this is a debug message");
-
     class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-            solution(32);
+            Console.WriteLine(solution(1041));
         }
 
-        public static int solution(int N)
-        {
-            // write your code in C# 6.0 with .NET 4.5 (Mono)
+        private static int solution(int N) {
             string nStr = Convert.ToString(N, 2);
             int gapLen = 0;
             var gaps = nStr.Split('1');
